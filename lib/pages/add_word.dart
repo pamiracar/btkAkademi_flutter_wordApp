29b49b66a1ae0c6bd75a734c8349b8f906269d67
pamiracar <acarpamir@gmail.com>
2025-06-08 00:53:20 +0300

    TextFields and DropdownMenu were created.

--- lib/pages/add_word.dart
+++ lib/pages/add_word.dart
@@ -8,13 +8,21 @@ class AddWordScreen extends StatefulWidget {
 }
 
 class _AddWordScreenState extends State<AddWordScreen> {
-
   final _formKey = GlobalKey<FormState>();
   final _englishController = TextEditingController();
   final _turkishController = TextEditingController();
   final _storyController = TextEditingController();
   String _selectedWordType = "Noun";
   bool isLearned = false;
+
+  List<String> wordType = [
+    "Noun",
+    "Adjective",
+    "Verv",
+    "Adverb",
+    "Phrasal Verb",
+    "Idiom",
+  ];
 
   @override
   void dispose() {
@@ -31,9 +39,46 @@ class _AddWordScreenState extends State<AddWordScreen> {
       child: Form(
         child: ListView(
           children: [
-            TextFormField(),
-            TextFormField(),
-            TextFormField(),
+            TextFormField(
+              controller: _englishController,
+              decoration: InputDecoration(
+                labelText: "English Word",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 10),
+            TextFormField(
+              controller: _turkishController,
+              decoration: InputDecoration(
+                labelText: "Turkish Word",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 10),
+            DropdownButtonFormField<String>(
+              decoration: InputDecoration(
+                border: OutlineInputBorder()
+              ),
+              items: wordType.map((e) {
+                return DropdownMenuItem<String>(
+                  value: e,
+                  child: Text(e),
+                );
+              }).toList(),
+              onChanged: (value) {
+                setState(() {
+                  _selectedWordType = value!;
+                });
+              },
+            ),SizedBox(height: 10,),
+            TextFormField(
+              controller: _storyController,
+              decoration: InputDecoration(
+                labelText: "Word Story",
+                border: OutlineInputBorder(),
+              ),
+              maxLines: 3,
+            ),
           ],
         ),
       ),
